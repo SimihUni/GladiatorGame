4528371cbca7532fd37240cad335c469a2173107
SimihUni <simih_school@abv.bg>
2022-08-03 12:55:50 +0300

    Added battle info display in battle_loop.

--- src/battle/battle_loop.rs
+++ src/battle/battle_loop.rs
@@ -1,7 +1,8 @@
 use crate::gladiator_struct::Gladiator;
 use crate::battle::battle_struct::Battle;
 
-pub fn battle_loop(battle_order: &u16, player: Gladiator){
+
+pub fn battle_loop(battle_order: &u16, player: Gladiator) {
     //idea: could add intro depending on what battle it is.
 
     //here should be a switch with enemies depending what battle order is, or randomized enemies made to match or challenge player
@@ -10,4 +11,27 @@ pub fn battle_loop(battle_order: &u16, player: Gladiator){
     //creating battle structure that controls the data that is relevent to the current battle.
     let mut battle_info = Battle::new(&player,&enemy);
     
+    //loop
+    loop {
+        //idea: add items, which means adding a currency, and menu for choosing items and so on
+        //idea: add move uses to balance strong moves
+        //idea: add battles against two enemies (not likely to happen)
+
+        //screen with battle stats
+        println!("{}",battle_info);
+
+        //choose attack
+        todo!();
+
+        //deal damage
+        todo!();
+
+        //check battle state
+        todo!();
+
+        //update turn variables
+        todo!();
+
+        //repeat
+    }
 }
--- src/battle/battle_struct.rs
+++ src/battle/battle_struct.rs
@@ -1,4 +1,5 @@
 use crate::gladiator_struct::Gladiator;
+use std::fmt;
 
 pub struct Battle {
     players_turn: bool, // is it the players turn
@@ -27,8 +28,25 @@ impl Battle {
         self.players_turn
     }
 
-    //set if it is the players turn
+    ///set if it is the players turn
     pub fn set_player_turn(&mut self, new_value: bool) {
         self.players_turn = new_value;
+    }
+
+    ///gets the name of this turn's gladiator
+    fn get_this_turn_name(&self) -> String {
+        if self.players_turn {
+            return self.player.get_name()
+        }
+        else {
+            return self.enemy.get_name()
+        }
+    }
+}
+
+impl fmt::Display for Battle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        f.write_fmt(format_args!("\n\t Battle between {} and {}\n\n\tPlayer HP:{}\n\tEnemy HP:{}\n\n\tIt is {}'s turn.",
+         self.player.get_name(), self.enemy.get_name(),self.player.get_hp(), self.enemy.get_hp(),self.get_this_turn_name()))
     }
 }
