c6664344b961a05b840eece47f63e2916ecc4c94
SimihUni <simih_school@abv.bg>
2022-09-03 09:41:19 +0300

    Updated help message.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use clap::Parser;
 author="\nAuthor:Simeon Mihaylov\n\n", 
 version,
 about="A small gladiator turn-based rpg game for the terminal.",
-long_about = "A small gladiator turn-based rpg game for the terminal. The goal of the game is to defeat the last boss named \"Boss Man\". The game uses numbered menus. The player can train up to five available moves, and train their 3 basic stats. The battle oponents are chosen randomly from a list, based on the current difficulty level, that changes through battle results. If the battle difficulty is high enough the last boss will be available to fight. Good Luck"
+long_about = "A small gladiator turn-based rpg game for the terminal. The goal of the game is to defeat the last boss named \"Boss Man\". The game uses numbered menus. The player can train up to five available moves, and train their 3 basic stats. The battle oponents are chosen randomly from a list, based on the current difficulty level, that changes through battle results. Every battle length as in turn is determined by the difficulty level. If the battle difficulty is high enough the last boss will be available to fight. Good Luck"
 )]
 struct Args {}
 
